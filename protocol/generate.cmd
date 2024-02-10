80c2dd9889312e8cbd216bfd7126737168a1b42c
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2024-02-10 19:04:35 +0300

    refactoring

--- protocol/generate.cmd
+++
@@ -1,2 +0,0 @@
-protoc -I=. --cpp_out=./erebus erebus.proto
-protoc -I=. --grpc_out=./erebus --plugin=protoc-gen-grpc="%gRPC_DIR%\..\..\tools\grpc\grpc_cpp_plugin.exe" erebus.proto
--- protocol/generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-protoc -I=. --cpp_out=./erebus erebus.proto
-protoc -I=. --grpc_out=./erebus --plugin=protoc-gen-grpc=${gRPC_DIR}/grpc_cpp_plugin erebus.proto
