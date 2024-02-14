5e3dbb71bd57a1bf3ab39de28862e4db84ade60a
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2024-02-14 12:15:32 +0300

    string utils

---
+++ include/erebus/util/stringutil.hxx
@@ -0,0 +1,182 @@
+#pragma once
+
+#include <erebus/erebus.hxx>
+
+#include <cwctype>
+#include <string_view>
+#include <vector>
+
+namespace Er
+{
+    
+namespace Util
+{
+    
+template <class StringT>
+std::vector<StringT> split(const StringT& s, const typename StringT::value_type* delimiter, size_t delimiterLength)
+{
+    size_t posStart = 0;
+    size_t posEnd = 0;
+
+    std::vector<StringT> result;
+    if (s.empty())
+        return result;
+
+    while ((posEnd = s.find(delimiter, posStart)) != StringT::npos)
+    {
+        auto token = s.substr(posStart, posEnd - posStart);
+        posStart = posEnd + delimiterLength;
+        result.emplace_back(std::move(token));
+    }
+
+    result.emplace_back(s.substr(posStart));
+    return result;
+}
+
+template <class StringT>
+std::vector<StringT> split(const StringT& s, typename StringT::value_type delimiter)
+{
+    const typename StringT::value_type d[] = { delimiter, typename StringT::value_type() };
+    return split(s, d, 1);
+}
+
+template <class StringT>
+void replace(StringT& s, typename StringT::value_type oldChar, typename StringT::value_type newChar)
+{
+    size_t pos = 0;
+    while ((pos = s.find(oldChar, pos)) != StringT::npos)
+    {
+        s.replace(pos, 1, 1, newChar);
+        pos += 1;
+    }
+}
+
+template <class StringT>
+StringT ltrim(const StringT& s)
+{
+    if constexpr (std::is_same_v<typename StringT::value_type, char>)
+    {
+        size_t start = s.find_first_not_of(" \n\r\t\f\v");
+        return (start == StringT::npos) ? StringT() : s.substr(start);
+    }
+    else if constexpr (std::is_same_v<typename StringT::value_type, wchar_t>)
+    {
+        size_t start = s.find_first_not_of(L" \n\r\t\f\v");
+        return (start == StringT::npos) ? StringT() : s.substr(start);
+    }
+    else
+    {
+        assert(!"Unsupported string type");
+    }
+}
+
+template <class StringT>
+StringT rtrim(const StringT& s)
+{
+    if constexpr (std::is_same_v<typename StringT::value_type, char>)
+    {
+        size_t end = s.find_last_not_of(" \n\r\t\f\v");
+        return (end == StringT::npos) ? StringT() : s.substr(0, end + 1);
+    }
+    else if constexpr (std::is_same_v<typename StringT::value_type, wchar_t>)
+    {
+        size_t end = s.find_last_not_of(L" \n\r\t\f\v");
+        return (end == StringT::npos) ? StringT() : s.substr(0, end + 1);
+    }
+    else
+    {
+        assert(!"Unsupported string type");
+    }
+}
+
+template <class StringT>
+StringT trim(const StringT& s)
+{
+    return rtrim(ltrim(s));
+}
+
+
+template <typename CharT>
+struct CharTraitsIgnoreCase;
+
+template <>
+struct CharTraitsIgnoreCase<char>
+    : public std::char_traits<char>
+{
+    static bool eq(char c1, char c2) { return std::toupper(c1) == std::toupper(c2); }
+    static bool ne(char c1, char c2) { return std::toupper(c1) != std::toupper(c2); }
+    static bool lt(char c1, char c2) { return std::toupper(c1) <  std::toupper(c2); }
+
+    static int compare(const char* s1, const char* s2, size_t n)
+    {
+        while (n-- != 0)
+        {
+            if (std::toupper(*s1) < std::toupper(*s2)) return -1;
+            if (std::toupper(*s1) > std::toupper(*s2)) return 1;
+
+            ++s1;
+            ++s2;
+        }
+
+        return 0;
+    }
+
+    static const char* find(const char* s, size_t n, char a)
+    {
+        while (n-- > 0)
+        {
+            if (std::toupper(*s) == std::toupper(a))
+                return s;
+
+            ++s;
+        }
+
+        return nullptr;
+    }
+};
+
+template <>
+struct CharTraitsIgnoreCase<wchar_t>
+    : public std::char_traits<wchar_t>
+{
+    static bool eq(wchar_t c1, wchar_t c2) { return std::towupper(c1) == std::towupper(c2); }
+    static bool ne(wchar_t c1, wchar_t c2) { return std::towupper(c1) != std::towupper(c2); }
+    static bool lt(wchar_t c1, wchar_t c2) { return std::towupper(c1) <  std::towupper(c2); }
+
+    static int compare(const wchar_t* s1, const wchar_t* s2, size_t n)
+    {
+        while (n-- != 0)
+        {
+            if (std::towupper(*s1) < std::towupper(*s2)) return -1;
+            if (std::towupper(*s1) > std::towupper(*s2)) return 1;
+
+            ++s1;
+            ++s2;
+        }
+
+        return 0;
+    }
+
+    static const wchar_t* find(const wchar_t* s, size_t n, wchar_t a)
+    {
+        while (n-- > 0)
+        {
+            if (std::towupper(*s) == std::towupper(a))
+                return s;
+                
+            ++s;
+        }
+
+        return nullptr;
+    }
+};
+
+
+template <typename CharT>
+using StringViewIgnoreCase = std::basic_string_view<CharT, CharTraitsIgnoreCase<CharT>>;
+
+
+    
+} // namespace Util {}
+    
+} // namespace Er {}
---
+++ tests/stringutil.cpp
@@ -0,0 +1,226 @@
+#include "common.hpp"
+
+#include <erebus/util/stringutil.hxx>
+
+
+TEST(string_split_by_char, char_string)
+{
+    // split empty string
+    {
+        std::string_view s;
+        auto v = Er::Util::split(s, ':');
+        EXPECT_TRUE(v.empty());
+    }
+
+    // unsplittable
+    {
+        std::string s("unsplittable string");
+        auto v = Er::Util::split(s, ':');
+        ASSERT_EQ(v.size(), 1);
+        EXPECT_STREQ(v.front().c_str(), s.c_str());
+    }
+
+    // empty parts
+    {
+        std::string s(":splittable string:");
+        auto v = Er::Util::split(s, ':');
+        ASSERT_EQ(v.size(), 3);
+        EXPECT_STREQ(v[0].c_str(), "");
+        EXPECT_STREQ(v[1].c_str(), "splittable string");
+        EXPECT_STREQ(v[2].c_str(), "");
+    }
+
+    // normal
+    {
+        std::string s("splittable string");
+        auto v = Er::Util::split(s, ' ');
+        ASSERT_EQ(v.size(), 2);
+        EXPECT_STREQ(v[0].c_str(), "splittable");
+        EXPECT_STREQ(v[1].c_str(), "string");
+    }
+}
+
+TEST(string_split_by_string, char_string)
+{
+    // split empty string
+    {
+        std::string_view s;
+        auto v = Er::Util::split(s, "::", 2);
+        EXPECT_TRUE(v.empty());
+    }
+
+    // unsplittable
+    {
+        std::string s("unsplittable string");
+        auto v = Er::Util::split(s, "::", 2);
+        ASSERT_EQ(v.size(), 1);
+        EXPECT_STREQ(v.front().c_str(), s.c_str());
+    }
+
+    // empty parts
+    {
+        std::string s("::splittable string::");
+        auto v = Er::Util::split(s, "::", 2);
+        ASSERT_EQ(v.size(), 3);
+        EXPECT_STREQ(v[0].c_str(), "");
+        EXPECT_STREQ(v[1].c_str(), "splittable string");
+        EXPECT_STREQ(v[2].c_str(), "");
+    }
+
+    // normal
+    {
+        std::string s("splittable::string");
+        auto v = Er::Util::split(s, "::", 2);
+        ASSERT_EQ(v.size(), 2);
+        EXPECT_STREQ(v[0].c_str(), "splittable");
+        EXPECT_STREQ(v[1].c_str(), "string");
+    }
+}
+
+TEST(string_split_by_char, wchar_string)
+{
+    // split empty string
+    {
+        std::wstring_view s;
+        auto v = Er::Util::split(s, L':');
+        EXPECT_TRUE(v.empty());
+    }
+
+    // unsplittable
+    {
+        std::wstring s(L"unsplittable string");
+        auto v = Er::Util::split(s, L':');
+        ASSERT_EQ(v.size(), 1);
+        EXPECT_STREQ(v.front().c_str(), s.c_str());
+    }
+
+    // empty parts
+    {
+        std::wstring s(L":splittable string:");
+        auto v = Er::Util::split(s, L':');
+        ASSERT_EQ(v.size(), 3);
+        EXPECT_STREQ(v[0].c_str(), L"");
+        EXPECT_STREQ(v[1].c_str(), L"splittable string");
+        EXPECT_STREQ(v[2].c_str(), L"");
+    }
+
+    // normal
+    {
+        std::wstring s(L"splittable string");
+        auto v = Er::Util::split(s, L' ');
+        ASSERT_EQ(v.size(), 2);
+        EXPECT_STREQ(v[0].c_str(), L"splittable");
+        EXPECT_STREQ(v[1].c_str(), L"string");
+    }
+}
+
+TEST(string_split_by_string, wchar_string)
+{
+    // split empty string
+    {
+        std::wstring_view s;
+        auto v = Er::Util::split(s, L"::", 2);
+        EXPECT_TRUE(v.empty());
+    }
+
+    // unsplittable
+    {
+        std::wstring s(L"unsplittable string");
+        auto v = Er::Util::split(s, L"::", 2);
+        ASSERT_EQ(v.size(), 1);
+        EXPECT_STREQ(v.front().c_str(), s.c_str());
+    }
+
+    // empty parts
+    {
+        std::wstring s(L"::splittable string::");
+        auto v = Er::Util::split(s, L"::", 2);
+        ASSERT_EQ(v.size(), 3);
+        EXPECT_STREQ(v[0].c_str(), L"");
+        EXPECT_STREQ(v[1].c_str(), L"splittable string");
+        EXPECT_STREQ(v[2].c_str(), L"");
+    }
+
+    // normal
+    {
+        std::wstring s(L"splittable::string");
+        auto v = Er::Util::split(s, L"::", 2);
+        ASSERT_EQ(v.size(), 2);
+        EXPECT_STREQ(v[0].c_str(), L"splittable");
+        EXPECT_STREQ(v[1].c_str(), L"string");
+    }
+}
+
+TEST(string_trim, char_string)
+{
+    // trim empty string
+    {
+        std::string s;
+        auto t = Er::Util::trim(s);
+        EXPECT_TRUE(t.empty());
+    }
+
+    // trim untrimmable
+    {
+        std::string s("untrimmable");
+        auto t = Er::Util::trim(s);
+        EXPECT_STREQ(t.c_str(), s.c_str());
+    }
+
+    // trim
+    {
+        std::string s(" \r\n\tnormal\n string\t \n ");
+        auto t = Er::Util::trim(s);
+        EXPECT_STREQ(t.c_str(), "normal\n string");
+    }
+}
+
+TEST(string_trim, wchar_string)
+{
+    // trim empty string
+    {
+        std::wstring s;
+        auto t = Er::Util::trim(s);
+        EXPECT_TRUE(t.empty());
+    }
+
+    // trim untrimmable
+    {
+        std::wstring s(L"untrimmable");
+        auto t = Er::Util::trim(s);
+        EXPECT_STREQ(t.c_str(), s.c_str());
+    }
+
+    // trim
+    {
+        std::wstring s(L" \r\n\tnormal\n string\t \n ");
+        auto t = Er::Util::trim(s);
+        EXPECT_STREQ(t.c_str(), L"normal\n string");
+    }
+}
+
+TEST(CharTraitsIgnoreCase, char_string)
+{
+    Er::Util::StringViewIgnoreCase s1("sample string");
+    Er::Util::StringViewIgnoreCase s2("SAMPLE STRING");
+    Er::Util::StringViewIgnoreCase s3("wrong string");
+
+    EXPECT_TRUE(s1 == s2);
+    EXPECT_FALSE(s1 == s3);
+    EXPECT_NE(s1.find("STRING"), s1.npos); 
+    EXPECT_NE(s2.find("string"), s2.npos);
+    EXPECT_EQ(s3.find("sample"), s3.npos);
+}
+
+TEST(CharTraitsIgnoreCase, wchar_string)
+{
+    Er::Util::StringViewIgnoreCase s1(L"sample string");
+    Er::Util::StringViewIgnoreCase s2(L"SAMPLE STRING");
+    Er::Util::StringViewIgnoreCase s3(L"wrong string");
+
+    EXPECT_TRUE(s1 == s2);
+    EXPECT_FALSE(s1 == s3);
+    EXPECT_NE(s1.find(L"STRING"), s1.npos);
+    EXPECT_NE(s2.find(L"string"), s2.npos);
+    EXPECT_EQ(s3.find(L"sample"), s3.npos);
+}
