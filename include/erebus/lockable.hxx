56394c0c2b7d205db067254b4942dc6f557f0388
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2024-02-26 10:55:02 +0300

    MutexPool + LockableObject

---
+++ include/erebus/lockable.hxx
@@ -0,0 +1,206 @@
+#pragma once
+
+#include <erebus/mutexpool.hxx>
+
+namespace Er
+{
+
+
+// 
+// object that has its own mutex
+//
+
+template <typename T, Mutex MutexT>
+class LockableObject
+    : public T
+{
+public:
+    using Base = T;
+    using MutexType = MutexT;
+
+    LockableObject() = default;
+
+    template <typename... Args>
+    LockableObject(MutexType& mutex, Args&&... args)
+        : T(std::forward<Args>(args)...)
+    {
+    }
+
+    // copying and moving are senseless for lockable objects
+    LockableObject(const LockableObject&) = delete;
+    LockableObject& operator=(const LockableObject&) = delete;
+
+    LockableObject(LockableObject&&) = delete;
+    LockableObject& operator=(LockableObject&&) = delete;
+
+    MutexType& mutex() const noexcept
+    {
+        return m_mutex;
+    }
+
+private:
+    mutable MutexType m_mutex;
+};
+
+
+// 
+// object that has an external mutex (suitable for use with a mutex pool)
+//
+
+template <typename T, Mutex MutexT>
+class ExternallyLockableObject
+    : public T
+{
+public:
+    using Base = T;
+    using MutexType = MutexT;
+
+    ExternallyLockableObject() = default;
+
+    template <typename... Args>
+    ExternallyLockableObject(MutexType& mutex, Args&&... args)
+        : T(std::forward<Args>(args)...)
+        , m_mutex(mutex)
+    {
+    }
+
+    // copying and moving are senseless for lockable objects
+    ExternallyLockableObject(const ExternallyLockableObject&) = delete;
+    ExternallyLockableObject& operator=(const ExternallyLockableObject&) = delete;
+
+    ExternallyLockableObject(ExternallyLockableObject&&) = delete;
+    ExternallyLockableObject& operator=(ExternallyLockableObject&&) = delete;
+
+    MutexType& mutex() const noexcept
+    {
+        return m_mutex;
+    }
+
+private:
+    MutexType& m_mutex;
+};
+
+
+template <typename T>
+concept Lockable =
+    requires(const T const_instance)
+    {
+        { const_instance.mutex() };
+        { const_instance.mutex().lock() };
+        { const_instance.mutex().unlock() };
+    };
+
+
+template <typename T>
+concept SharedlyLockable =
+    requires(const T const_instance)
+    {
+        { const_instance.mutex() };
+        { const_instance.mutex().lock_shared() };
+        { const_instance.mutex().unlock_shared() };
+    };
+
+
+//
+// acquire an exclusive access to the underlying object
+//
+
+template <Lockable ObjectT>
+class ObjectLock
+    : public NonCopyable
+{
+public:
+    using ObjectType = ObjectT;
+
+    ~ObjectLock()
+    {
+        m_object->mutex().unlock();
+    }
+
+    explicit ObjectLock(ObjectType& o) noexcept
+        : m_object(&o)
+    {
+        m_object->mutex().lock();
+    }
+
+    explicit ObjectLock(ObjectType* o) noexcept
+        : m_object(o)
+    {
+        assert(m_object);
+        m_object->mutex().lock();
+    }
+
+    [[nodiscard]] constexpr const ObjectType* get() const noexcept
+    {
+        return m_object;
+    }
+
+    [[nodiscard]] constexpr const ObjectType* get() noexcept
+    {
+        return m_object;
+    }
+
+    [[nodiscard]] constexpr const ObjectType* operator->() const noexcept
+    {
+        assert(m_object);
+        return m_object;
+    }
+
+    [[nodiscard]] constexpr ObjectType* operator->() noexcept
+    {
+        assert(m_object);
+        return m_object;
+    }
+
+private:
+    ObjectType* m_object = nullptr;
+};
+
+
+//
+// acquire a shared access to the underlying object
+//
+
+template <SharedlyLockable ObjectT>
+class SharedObjectLock
+    : public NonCopyable
+{
+public:
+    using ObjectType = ObjectT;
+
+    ~SharedObjectLock()
+    {
+        m_object->mutex().unlock_shared();
+    }
+
+    explicit SharedObjectLock(const ObjectType& o) noexcept
+        : m_object(&o)
+    {
+        m_object->mutex().lock_shared();
+    }
+
+    explicit SharedObjectLock(const SharedObjectLock* o) noexcept
+        : m_object(o)
+    {
+        assert(m_object);
+        m_object->mutex().lock_shared();
+    }
+
+    [[nodiscard]] constexpr const ObjectType* get() const noexcept
+    {
+        return m_object;
+    }
+
+    [[nodiscard]] constexpr const ObjectType* operator->() const noexcept
+    {
+        assert(m_object);
+        return m_object;
+    }
+
+private:
+    // shared access implies read-only
+    const ObjectType* m_object = nullptr;
+};
+
+
+} // namespace Er {}
---
+++ include/erebus/mutexpool.hxx
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <erebus/erebus.hxx>
+
+#include <atomic>
+#include <vector>
+
+namespace Er
+{
+
+
+template <class MutexT>
+concept Mutex = 
+    requires(MutexT m)
+    {
+        { m.lock() };
+        { m.unlock() };
+    };
+
+template <class MutexT>
+concept SharedMutex = 
+    requires(MutexT m)
+    {
+        { m.lock() };
+        { m.unlock() };
+        { m.lock_shared() };
+        { m.unlock_shared() };
+    };
+
+
+//
+// mutex pool is useful when we have a huge bunch of objects
+// each of which needs to be independently accessed in a thread-safe manner;
+// creating one mutex per object is too expensive in this case;
+// given that any thread accesses one object at a time most of the time, we can
+// maintain a relatively small pool of mutexes and assign them to our objects in a
+// round-robin manner so that each mutex protects many objects without a significant performance impact
+//
+
+
+template <class MutexT>
+class MutexPool;
+
+
+template <Mutex MutexT>
+class MutexPool<MutexT> final
+    : public NonCopyable
+{
+public:
+    using MutexType = MutexT;
+
+    explicit MutexPool(std::size_t count)
+        : m_mutices(count)
+    {
+        assert(count > 0);
+    }
+
+    [[nodiscard]] MutexType& mutex() noexcept
+    {
+        std::size_t index;
+        std::size_t expected;
+        do
+        {
+            expected = m_next.load(std::memory_order_relaxed);
+            index = expected + 1;
+            if (index == m_mutices.size())
+                index = 0;
+
+        } while (!m_next.compare_exchange_strong(expected, index, std::memory_order_acq_rel));
+        
+        return m_mutices[index];
+    }
+
+private:
+    std::atomic<std::size_t> m_next = 0;
+    std::vector<MutexType> m_mutices;
+};
+
+
+
+} // namespace Er {}
