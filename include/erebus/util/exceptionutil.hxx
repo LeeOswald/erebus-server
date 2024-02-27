56c6bb3720f35239490068989f470fdec35f6f58
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2024-02-27 11:06:33 +0300

    Er::protectedCall() added

--- include/erebus/util/exceptionutil.hxx
+++ include/erebus/util/exceptionutil.hxx
@@ -18,5 +18,39 @@ void EREBUS_EXPORT logException(Log::ILog* log, Log::Level level, const Er::Exce
 
 } // namespace Util {}
 
+
+template <typename ResultT, typename WorkT, typename... Args>
+ResultT protectedCall(Er::Log::ILog* log, const Er::Log::Location& location, WorkT work, Args&&... args)
+{
+    try
+    {
+        return work(std::forward<Args>(args)...);
+    }
+    catch (Er::Exception& e)
+    {
+        if (log)
+        {
+            auto msg = Er::Util::formatException(e);
+            log->write(Er::Log::Level::Error, location, "%s", msg.c_str());
+        }
+    }
+    catch (std::exception& e)
+    {
+        if (log)
+        {
+            auto msg = Er::Util::formatException(e);
+            log->write(Er::Log::Level::Error, location, "%s", msg.c_str());
+        }
+    }
+    catch (...)
+    {
+        if (log)
+            log->write(Er::Log::Level::Error, location, "Unexpected exception");
+    }
+
+    return ResultT();
+}
+
+
 } // namespace Er {}
 
