b3d6f84988bcd63e78486a5b278f57c20e38b1a4
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2024-10-21 09:48:59 +0300

    use [fmt] lib

--- src/erebus/debug.cxx
+++ src/erebus/debug.cxx
@@ -67,43 +67,15 @@ static void trace_impl(const std::string& text)
 
 #endif
 
-static void tracev(const char* format, va_list args) noexcept
-{
-    try
-    {
-        va_list args1;
-        va_copy(args1, args);
-        auto required = ::vsnprintf(nullptr, 0, format, args1);
 
-        std::string formatted;
-        formatted.resize(required);
-        ::vsnprintf(formatted.data(), required + 1, format, args);
-
-        va_end(args1);
-
-        return trace_impl(formatted);
-    }
-    catch (...)
-    {
-    }
-}
-
-EREBUS_EXPORT void trace(const char* format, ...) noexcept
+EREBUS_EXPORT void trace(std::string&& message) noexcept
 {
 #if ER_WINDOWS
     if (!::IsDebuggerPresent())
         return;
 #endif
 
-    if (!format || !*format)
-        return;
-
-    va_list args;
-    va_start(args, format);
-
-    tracev(format, args);
-
-    va_end(args);
+    trace_impl(message);
 }
 
 } // namespace Er {}
